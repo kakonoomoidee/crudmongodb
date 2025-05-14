859206fd998e39f0db5a14941a451f9e4e45f1da
kakonoomoidee <rizki.ramadan.ft22@mail.umy.ac.id>
2025-05-14 10:45:48 +0700

    Refactor MongoDB connection string and update formatting in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const itemRoutes = require('./routes/itemRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const itemRoutes = require("./routes/itemRoutes");
 
 const app = express();
 const port = 3000;
@@ -11,15 +11,16 @@ app.use(bodyParser.json());
 
 // MongoDB Connection
 // Note: Ubah URL koneksi sesuai dengan konfigurasi MongoDB Anda caranya tinggal masuk "mongosh" di terminal
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.1', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/CrudPert6", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 // Routes
-app.use('/api/items', itemRoutes);
+app.use("/api/items", itemRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
